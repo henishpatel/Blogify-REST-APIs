88b6a65c63aba34894ad76e4755406afd31c4966
Henish <henish134@gmail.com>
2023-09-02 16:31:50 -0400

    Starter code + Database connection

---
+++ src/test/java/com/henishpatel/bloggingapplication/BloggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henishpatel.bloggingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
