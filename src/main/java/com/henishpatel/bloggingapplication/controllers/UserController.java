d8d965a6b7dee1f304f797476b2f6662d64f766a
Henish <henish134@gmail.com>
2023-09-04 14:19:38 -0400

    Used ModelMapper to convert User to UserDTO and vice versa

--- src/main/java/com/henishpatel/bloggingapplication/controllers/UserController.java
+++ src/main/java/com/henishpatel/bloggingapplication/controllers/UserController.java
@@ -1,11 +1,16 @@
 package com.henishpatel.bloggingapplication.controllers;
 
+import com.henishpatel.bloggingapplication.entities.User;
+import com.henishpatel.bloggingapplication.payload.ApiResponse;
 import com.henishpatel.bloggingapplication.payload.UserDTO;
 import com.henishpatel.bloggingapplication.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
@@ -17,12 +22,32 @@ public class UserController {
 	// POST - create user
 	@PostMapping("/")
 	public ResponseEntity<UserDTO> createUser(@RequestBody UserDTO userDTO){
-		UserDTO createUserDTO = this.userService.createUser(userDTO);
+		UserDTO createUserDTO = userService.createUser(userDTO);
 		return new ResponseEntity<>(createUserDTO, HttpStatus.CREATED);
 	}
 	// PUT - update user
-	// DELETE - delete user
-	// GET - get user
+	@PutMapping("/{userId}")
+	public ResponseEntity<UserDTO> updateUser(@RequestBody UserDTO userDTO,@PathVariable Integer userId ){
+		UserDTO updateUserDTO = userService.updateUser(userDTO,userId);
+		return ResponseEntity.ok(updateUserDTO);
+	}
 
+	// DELETE - delete user
+	@DeleteMapping("/{userId}")
+	public ResponseEntity<ApiResponse> deleteUser(@PathVariable Integer userId){
+		userService.deleteUser(userId);
+		return new ResponseEntity<>(new ApiResponse("User deleted Successfully",true),HttpStatus.OK);
+	}
+
+	// GET - get user
+	@GetMapping("/")
+	public ResponseEntity<List<UserDTO>> getAllUsers(){
+		return ResponseEntity.ok(userService.getAllUsers());
+	}
+
+	@GetMapping("/{userId}")
+	public ResponseEntity<UserDTO> getUserById(@PathVariable Integer userId){
+		return ResponseEntity.ok(userService.getUserById(userId));
+	}
 
 }
--- src/main/java/com/henishpatel/bloggingapplication/services/impl/UserServiceImpl.java
+++ src/main/java/com/henishpatel/bloggingapplication/services/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.henishpatel.bloggingapplication.exceptions.ResourceNotFoundException;
 import com.henishpatel.bloggingapplication.payload.UserDTO;
 import com.henishpatel.bloggingapplication.repositories.UserRepo;
 import com.henishpatel.bloggingapplication.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,9 @@ public class UserServiceImpl implements UserService {
 
 	@Autowired
 	private UserRepo userRepo;
+	@Autowired
+	private ModelMapper modelMapper;
+
 	@Override
 	public UserDTO createUser(UserDTO userDTO) {
 		User user = this.dtoToUser(userDTO);
@@ -59,23 +63,26 @@ public class UserServiceImpl implements UserService {
 
 	public User dtoToUser(UserDTO userDTO) {
 
-		User user = new User();
-		user.setId(userDTO.getId());
-		user.setName(userDTO.getName());
-		user.setEmail(userDTO.getEmail());
-		user.setAbout(userDTO.getAbout());
-		user.setPassword(userDTO.getPassword());
+		User user = modelMapper.map(userDTO,User.class);
+
+//		user.setId(userDTO.getId());
+//		user.setName(userDTO.getName());
+//		user.setEmail(userDTO.getEmail());
+//		user.setAbout(userDTO.getAbout());
+//		user.setPassword(userDTO.getPassword());
+
 		return user;
 	}
 
 	public UserDTO userToDto(User user) {
 
-		UserDTO userDTO = new UserDTO();
-		userDTO.setId(user.getId());
-		userDTO.setName(user.getName());
-		userDTO.setEmail(user.getEmail());
-		userDTO.setAbout(user.getAbout());
-		userDTO.setPassword(user.getPassword());
+		UserDTO userDTO = modelMapper.map(user,UserDTO.class);
+
+//		userDTO.setId(user.getId());
+//		userDTO.setName(user.getName());
+//		userDTO.setEmail(user.getEmail());
+//		userDTO.setAbout(user.getAbout());
+//		userDTO.setPassword(user.getPassword());
 		return userDTO;
 	}
 }
