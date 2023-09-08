5ef5fe4995d1224870b44327f59c8b900366e05c
Henish <henish134@gmail.com>
2023-09-08 02:03:13 -0400

    Code optimize

--- src/main/java/com/henishpatel/bloggingapplication/config/SecurityConfig.java
+++ src/main/java/com/henishpatel/bloggingapplication/config/SecurityConfig.java
@@ -3,26 +3,17 @@ package com.henishpatel.bloggingapplication.config;
 import com.henishpatel.bloggingapplication.security.CustomUserDetailsService;
 import com.henishpatel.bloggingapplication.security.JwtAuthenticationEntryPoint;
 import com.henishpatel.bloggingapplication.security.JwtAuthenticationFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -33,18 +24,17 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
-	@Autowired
-	private UserDetailsService userDetailsService;
+	private final CustomUserDetailsService customUserDetailsService;
 
-	@Autowired
-	private CustomUserDetailsService customUserDetailsService;
+	private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
-	@Autowired
-	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+	private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-	@Autowired
-	private JwtAuthenticationFilter jwtAuthenticationFilter;
-
+	public SecurityConfig(CustomUserDetailsService customUserDetailsService, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint, JwtAuthenticationFilter jwtAuthenticationFilter) {
+		this.customUserDetailsService = customUserDetailsService;
+		this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+		this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+	}
 
 
 	@Bean
@@ -76,25 +66,6 @@ public class SecurityConfig {
 		http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
-//		httpSecurity.csrf()
-//				.disable()
-//				.authorizeHttpRequests(
-//						(authorize) -> authorize
-//								.requestMatchers(HttpMethod.POST,"/api/v1/auth/login").permitAll()
-//								.anyRequest()
-//								.authenticated()
-//
-//				).exceptionHandling(exception -> exception
-//						.authenticationEntryPoint(jwtAuthenticationEntryPoint)
-//				).sessionManagement( session -> session
-//						.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//				);
-//
-//		httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-//
-//		httpSecurity.authenticationProvider(daoAuthenticationProvider());
-//
-//		return httpSecurity.build();
 	}
 
 	@Bean
